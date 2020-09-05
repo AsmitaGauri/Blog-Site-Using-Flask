13cde6ae261c2954e96f357e2783b06086ff3234
Asmita Gauri <asmitagaurikhan@gmail.com>
2020-09-06 01:12:34 +0530

    Implemented Login and Register Form using wtf_forms

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,7 +1,15 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+# url_for accepts the name of the function you want to pass the control to
+from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
 # initializing flask
 
+
+# to prevent the forms from forgery attacks we need to provide a secret key
+# in cmd line
+# import secrets
+# secrets.token_hex(16)
+app.config['SECRET_KEY']='9ae07d1837a5a3333786463b73ab6d0c'
 
 posts=[
     {
@@ -27,6 +35,29 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=='admin@gmail.com' and form.password.data=='password':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsucessful, Please check your password and email again','danger')
+    return render_template('login.html',title='Login',form=form)
+
 
 if __name__=='__main__':
     app.run(debug=True)
