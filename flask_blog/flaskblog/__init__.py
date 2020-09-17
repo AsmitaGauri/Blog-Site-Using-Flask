ee9b7e57f10f739fccee3a7c4d2b2eda7c78c162
Asmita Gauri <asmitagaurikhan@gmail.com>
2020-09-17 14:51:01 +0530

    User Authentication

--- flask_blog/flaskblog/__init__.py
+++ flask_blog/flaskblog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 # url_for accepts the name of the function you want to pass the control to
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 app=Flask(__name__)
 # initializing flask
 
@@ -16,4 +18,8 @@ app.config['SECRET_KEY']='9ae07d1837a5a3333786463b73ab6d0c'
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///site.db"
 
 db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category="info"
 from flaskblog import routes
--- flask_blog/flaskblog/models.py
+++ flask_blog/flaskblog/models.py
@@ -1,9 +1,14 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db,login_manager
+from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def login_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+
+class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(20),unique=True,nullable=False)
     email=db.Column(db.String(120),unique=True,nullable=False)
@@ -12,6 +17,10 @@ class User(db.Model):
     posts=db.relationship('Post',backref="author",lazy=True) #It just defines the background relationship
     def __repr__(self):
         return f"User('${self.username}','${self.email}','${self.image_file}')"
+
+    
+   
+      
 
 
 class Post(db.Model):
