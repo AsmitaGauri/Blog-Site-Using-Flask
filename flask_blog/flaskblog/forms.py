3b42f5d7365565e95cb13c8d8bf9132fd5ad7d96
Asmita Gauri <asmitagaurikhan@gmail.com>
2020-10-26 13:44:16 +0530

    Final Functionality done

--- flask_blog/flaskblog/forms.py
+++ flask_blog/flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
-from wtforms import StringField,SubmitField,PasswordField,BooleanField
+from wtforms import StringField,SubmitField,PasswordField,BooleanField,TextAreaField
+
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flaskblog.models import User
 from flask_login import current_user
@@ -59,3 +60,8 @@ class UpdateAccountForm(FlaskForm):
             user=User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Email exists, Please choose a different one!')
+
+class PostForm(FlaskForm):
+    title=StringField('Title',validators=[DataRequired()])
+    content=TextAreaField('Content',validators=[DataRequired()])
+    submit=SubmitField('Post')
